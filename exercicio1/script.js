ea54cd67990ad8f564391ab7dd2950822cffe544
EvertonSouzaFerreira <everton.souzaferreira@outlook.com>
2022-08-30 19:55:04 +0200

    Aula02-js

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeroPar = prompt('insira um numero par')
+
+let resposta =  Number(numeroPar) 
+
+console.log(resposta%2) // notei que o resto da divisao por 2 de um numero par e sempre 0
+
+
+                        // se o numero for impar vamos tem um resutado 1
